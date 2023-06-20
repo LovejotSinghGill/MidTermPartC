da4ebd354ce209c8e8f7e719e785c8f1eef232de
LovejotSinghGill <133809229+LovejotSinghGill@users.noreply.github.com>
2023-06-20 14:57:30 -0400

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -11,8 +11,8 @@ package midtermexam_versiona_extensioncode;
  * genres array. This code is to be used for SYST 17796 midterm exam.
  * Students are reminded to add themselves as modifiers when editing the
  * code.
- * @author dancye, 2019
- */
+ * @author Lovejot Singh gill 2023
+ */ comment is added
 public class UserProfile 
 {
     
@@ -66,3 +66,5 @@ public class UserProfile
     
     
 }//end class
+
+
